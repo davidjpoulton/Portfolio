34c2dbe5d82a3a9721947f7a163ccedd6bfbb1aa
David Poulton <davidpoulton123@hotmail.co.uk>
2020-01-29 20:45:00 +0000

    -completed code box on home page. -added a seperate home page with animations so when youu go back home you dont have to wait for the animations again.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
+
+// function to fadeout the floating box on click and fade in the code box
 $(document).ready(function(){
     $(".box").click(function(){
-        $("#animated-box").fadeOut();
-        $("#box-text").fadeOut();
-        $(".code-box").delay(600).fadeIn();
+        $("#animated-box").fadeToggle();
+        $("#box-text").fadeToggle();
+        $("#css-box").delay(600).fadeToggle();
     });
 });
 
+//function to let the user go back to the floating box if the arrow is pressed
 $(document).ready(function(){
-    $("#box-header").click(function(){
-        $("#animated-box").fadeIn();
-        $(".code-box").fadeOut();
+    $(".back-arrow").click(function(){
+        $("#animated-box").fadeIn("slow");
+        $("#box-text").fadeIn();  
+        $("#css-box").fadeOut(); 
+        $("#html-box").fadeOut(); 
+        $("#js-box").fadeOut(); 
     });
 });
+
+function toggleHtml() {
+    let cssBox = document.getElementById("css-box")
+    let htmlBox = document.getElementById("html-box");
+    let jsBox = document.getElementById("js-box");
+
+    if (htmlBox.style.display = "none") {
+      htmlBox.style.display = "block";
+      cssBox.style.display = "none";
+      jsBox.style.display = "none";
+    } 
+}
+
+function toggleCss() {
+    let cssBox = document.getElementById("css-box")
+    let htmlBox = document.getElementById("html-box");
+    let jsBox = document.getElementById("js-box");
+
+    if (cssBox.style.display = "none") {
+      htmlBox.style.display = "none";
+      cssBox.style.display = "block";
+      jsBox.style.display = "none";
+    } 
+}
+
+function toggleJs() {
+    let cssBox = document.getElementById("css-box")
+    let htmlBox = document.getElementById("html-box");
+    let jsBox = document.getElementById("js-box");
+
+    if (jsBox.style.display = "none") {
+      htmlBox.style.display = "none";
+      cssBox.style.display = "none";
+      jsBox.style.display = "block";
+    } 
+}
 
 
