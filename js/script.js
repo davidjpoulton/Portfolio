d8cce76efc3053a9e4f6ed4be89e5012effcef52
David Poulton <davidpoulton123@hotmail.co.uk>
2020-05-12 00:12:15 +0100

    -added shortcut menu to all pages.

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let list1             = document.querySelector("#list1");
 let list2             = document.querySelector("#list2");
 let list3             = document.querySelector("#list3");
 let list4             = document.querySelector("#list4");
-
+let withMe            = document.querySelector("#social-with-me")
 
 function makeVisible(){
     navSocialDropDown.style.transitionDelay = "0s";
@@ -46,33 +46,50 @@ function hideSocialMenu(){
 }
 
 function invertArrows() {
+    withMe.style.opacity = "0";
+    withMe.style.transition = "all .2s ease";
+    withMe.style.transitionDelay = ".4s";
     arrowLeft.style.transform = "rotate(-150deg)";
     arrowLeft.style.background = "#55FFEB";
+    arrowLeft.style.animation = "moveLeft .7s"
+    arrowLeft.style.animationDelay = ".4s"
+    arrowLeft.style.animationFillMode = "forwards"
+    arrowRight.style.animation = "moveLeft .7s"
+    arrowRight.style.animationDelay = ".4s"
+    arrowRight.style.animationFillMode = "forwards"
     arrowRight.style.transform = "rotate(150deg)";
     arrowRight.style.background = "#55FFEB";
 }
 
 function invertBack() {
-    arrowLeft.style.transform = "rotate(-30deg)";
-    arrowLeft.style.top = "-19px";
+    withMe.style.opacity = "1";
+    withMe.style.transitionDelay = ".6s";
+    arrowLeft.style.transform = "rotate(-90deg)";
     arrowLeft.style.background = "rgb(112, 112, 112)";
-    arrowRight.style.transform = "rotate(30deg)";
+    arrowRight.style.transform = "rotate(90deg)";
     arrowRight.style.background = "rgb(112, 112, 112)";
 }
 
-
-
+function moveArrowsBack() {
+    arrowLeft.style.animation = "moveBack 1s ease";
+    arrowRight.style.animation = "moveBack 1s ease";
+    arrowRight.style.animationFillMode = "forwards";
+    arrowLeft.style.animationFillMode = "forwards";
+}
 
 navSocial.addEventListener("mouseover", () => {
     invertArrows();
     makeVisible();
-    moveArrows();
 });
 
 navSocial.addEventListener("mouseleave", () => {
     invertBack();
     hideSocialMenu();
 });
+
+navSocial.addEventListener("mouseleave", () => {
+    moveArrowsBack();
+} ) 
 
 
 
