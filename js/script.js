059cf901a98c54d12c3dd34fda83c26b9aee9046
David Poulton <davidpoulton123@hotmail.co.uk>
2020-05-12 15:54:41 +0100

    -changed my work page to have an interactive element

--- js/script.js
+++ js/script.js
@@ -92,6 +92,215 @@ navSocial.addEventListener("mouseleave", () => {
 } ) 
 
 
+//Work page sliding element
+
+let img1 = document.querySelector(".img1")
+let img2 = document.querySelector(".img2")
+let img3 = document.querySelector(".img3")
+let img4 = document.querySelector(".img4")
+let img5 = document.querySelector(".img5")
+let img6 = document.querySelector(".img6")
+let scrollLeft = document.querySelector("#arrow-back")
+let scrollRight = document.querySelector("#arrow-forward")
+
+scrollLeft.addEventListener("click", shiftLeft);
+scrollRight.addEventListener("click", shiftRight);
+
+window.addEventListener('wheel', function(event)
+{
+ if (event.deltaY < 0)
+ {
+  shiftLeft();
+ }
+ else if (event.deltaY > 0)
+ {
+  shiftRight();
+ }
+});
+
+function shiftLeft () {
+    if (img1.classList.contains("pos1")) {
+        img1.classList.add("pos6");
+        img1.classList.remove("pos1");
+        img2.classList.add("pos1");
+        img2.classList.remove("pos2");
+        img3.classList.add("pos2");
+        img3.classList.remove("pos3");
+        img4.classList.add("pos3");
+        img4.classList.remove("pos4");
+        img5.classList.add("pos4");
+        img5.classList.remove("pos5");
+        img6.classList.add("pos5");
+        img6.classList.remove("pos6");
+    }  
+
+    else if (img1.classList.contains("pos6")) {
+        img1.classList.add("pos5");
+        img1.classList.remove("pos6");
+        img2.classList.add("pos6");
+        img2.classList.remove("pos1");
+        img3.classList.add("pos1");
+        img3.classList.remove("pos2");
+        img4.classList.add("pos2");
+        img4.classList.remove("pos3");
+        img5.classList.add("pos3");
+        img5.classList.remove("pos4");
+        img6.classList.add("pos4");
+        img6.classList.remove("pos5");
+    }
+
+    else if (img1.classList.contains("pos5")) {
+        img1.classList.add("pos4")
+        img1.classList.remove("pos5")
+        img2.classList.add("pos5")
+        img2.classList.remove("pos6")
+        img3.classList.add("pos6")
+        img3.classList.remove("pos1")
+        img4.classList.add("pos1")
+        img4.classList.remove("pos2")
+        img5.classList.add("pos2")
+        img5.classList.remove("pos3")
+        img6.classList.add("pos3")
+        img6.classList.remove("pos4")
+    }
+
+    else if (img1.classList.contains("pos4")) {
+        img1.classList.add("pos3")
+        img1.classList.remove("pos4")
+        img2.classList.add("pos4")
+        img2.classList.remove("pos5")
+        img3.classList.add("pos5")
+        img3.classList.remove("pos6")
+        img4.classList.add("pos6")
+        img4.classList.remove("pos1")
+        img5.classList.add("pos1")
+        img5.classList.remove("pos2")
+        img6.classList.add("pos2")
+        img6.classList.remove("pos3")
+    }
+
+    else if (img1.classList.contains("pos3")) {
+        img1.classList.add("pos2")
+        img1.classList.remove("pos3")
+        img2.classList.add("pos3")
+        img2.classList.remove("pos4")
+        img3.classList.add("pos4")
+        img3.classList.remove("pos5")
+        img4.classList.add("pos5")
+        img4.classList.remove("pos6")
+        img5.classList.add("pos6")
+        img5.classList.remove("pos1")
+        img6.classList.add("pos1")
+        img6.classList.remove("pos2")
+    }
+
+    else if (img1.classList.contains("pos2")) {
+        img1.classList.add("pos1")
+        img1.classList.remove("pos2")
+        img2.classList.add("pos2")
+        img2.classList.remove("pos3")
+        img3.classList.add("pos3")
+        img3.classList.remove("pos4")
+        img4.classList.add("pos4")
+        img4.classList.remove("pos5")
+        img5.classList.add("pos5")
+        img5.classList.remove("pos6")
+        img6.classList.add("pos6")
+        img6.classList.remove("pos1")
+    }
+}
+
+function shiftRight () {
+    if (img1.classList.contains("pos1")) {
+        img1.classList.add("pos2");
+        img1.classList.remove("pos1");
+        img2.classList.add("pos3");
+        img2.classList.remove("pos2");
+        img3.classList.add("pos4");
+        img3.classList.remove("pos3");
+        img4.classList.add("pos5");
+        img4.classList.remove("pos4");
+        img5.classList.add("pos6");
+        img5.classList.remove("pos5");
+        img6.classList.add("pos1");
+        img6.classList.remove("pos6");
+    }  
+
+    else if (img1.classList.contains("pos2")) {
+        img1.classList.add("pos3");
+        img1.classList.remove("pos2");
+        img2.classList.add("pos4");
+        img2.classList.remove("pos3");
+        img3.classList.add("pos5");
+        img3.classList.remove("pos4");
+        img4.classList.add("pos6");
+        img4.classList.remove("pos5");
+        img5.classList.add("pos1");
+        img5.classList.remove("pos6");
+        img6.classList.add("pos2");
+        img6.classList.remove("pos1");
+    }
+
+    else if (img1.classList.contains("pos3")) {
+        img1.classList.add("pos4")
+        img1.classList.remove("pos3")
+        img2.classList.add("pos5")
+        img2.classList.remove("pos4")
+        img3.classList.add("pos6")
+        img3.classList.remove("pos5")
+        img4.classList.add("pos1")
+        img4.classList.remove("pos6")
+        img5.classList.add("pos2")
+        img5.classList.remove("pos1")
+        img6.classList.add("pos3")
+        img6.classList.remove("pos2")
+    }
+
+    else if (img1.classList.contains("pos4")) {
+        img1.classList.add("pos5")
+        img1.classList.remove("pos4")
+        img2.classList.add("pos6")
+        img2.classList.remove("pos5")
+        img3.classList.add("pos1")
+        img3.classList.remove("pos6")
+        img4.classList.add("pos2")
+        img4.classList.remove("pos1")
+        img5.classList.add("pos3")
+        img5.classList.remove("pos2")
+        img6.classList.add("pos4")
+        img6.classList.remove("pos3")
+    }
+
+    else if (img1.classList.contains("pos5")) {
+        img1.classList.add("pos6")
+        img1.classList.remove("pos5")
+        img2.classList.add("pos1")
+        img2.classList.remove("pos6")
+        img3.classList.add("pos2")
+        img3.classList.remove("pos1")
+        img4.classList.add("pos3")
+        img4.classList.remove("pos2")
+        img5.classList.add("pos4")
+        img5.classList.remove("pos3")
+        img6.classList.add("pos5")
+        img6.classList.remove("pos4")
+    }
+
+    else if (img1.classList.contains("pos6")) {
+        img1.classList.add("pos1")
+        img1.classList.remove("pos6")
+        img2.classList.add("pos2")
+        img2.classList.remove("pos1")
+        img3.classList.add("pos3")
+        img3.classList.remove("pos2")
+        img4.classList.add("pos4")
+        img4.classList.remove("pos3")
+        img5.classList.add("pos5")
+        img5.classList.remove("pos4")
+        img6.classList.add("pos6")
+        img6.classList.remove("pos5 ")
+    }
+}
 
 // JQUERY
 // function to fadeout the floating box on click and fade in the code box
